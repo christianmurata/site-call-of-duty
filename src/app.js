80124ff7e2188303b6acbd9f883260fc237888e5
Christian Murata <chris_murata@hotmail.com>
2021-07-24 18:34:19 -0300

    add: base scripts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 (function(){
-  const router = new Router ([
+  document.addEventListener('submit', function(e) {
+    e.preventDefault()
+    console.log(e.target);
+
+    alert('dadsa');
+  });
+
+  new Router ([
     new Route('main', 'main.html', true),
-    new Route('login', 'login.html')
+    new Route('login', 'login.html'),
+    new Route('dashboard', 'dashboard.html'),
   ]);
 }());
