891010f67f517b6fab32b5d9537f6887abbdf1ab
Christian Murata <chris_murata@hotmail.com>
2021-07-24 16:17:43 -0300

    feat: single-page application

---
+++ src/Route.js
@@ -0,0 +1,11 @@
+class Route {
+  constructor(name, html, defaultRoute = false) {
+    this.name = name;
+    this.html = html;
+    this.defaultRoute = defaultRoute;
+  }
+
+  isActive(pathHash) {
+    return pathHash.replace('#', '') === this.name;
+  }
+}
---
+++ src/app.js
@@ -0,0 +1,6 @@
+(function(){
+  const router = new Router ([
+    new Route('main', 'main.html', true),
+    new Route('login', 'login.html')
+  ]);
+}());
