c6a8ca664d95c96ef7470950f7cba03a868adef6
Christian Murata <chris_murata@hotmail.com>
2021-08-29 18:43:03 -0300

    add: UI/UX

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,32 +2,47 @@ class Header {
   constructor() {
     this.play = document.getElementById('play');
     this.header = document.getElementById('header');
-    this.logout = document.getElementById('logout');
+    this.postButton = document.getElementById('post');
     this.logoutButton = document.getElementById('logout');
 
     this.init();
   }
 
   init() {
-    this.logoutButton.addEventListener('click', (e) => this.sessionLogout(this))
+    if(this.postButton.onclick) return false;
+
+    this.postButton.addEventListener('click', (e) => this.newPost(e));
+    this.logoutButton.addEventListener('click', (e) => this.sessionLogout(this));
+
+    this.postButton.onclick = () => true;
   }
 
   userHeader() {
     this.play.style.display = 'none';
-    this.logout.style.display = 'block';
     this.visibility(this.header.getElementsByClassName('item'), 'none');
+    this.visibility(this.header.getElementsByClassName('logged-in'), 'block');
   }
 
   visitorHeader() {
     this.play.style.display = 'block';
-    this.logout.style.display = 'none';
     this.visibility(this.header.getElementsByClassName('item'), 'block');
+    this.visibility(this.header.getElementsByClassName('logged-in'), 'none');
   }
 
   visibility(itens, display) {
     for (let item of itens) {
       item.style.display = display;
     }
+  }
+
+  newPost(e) {
+    e.preventDefault();
+
+    if(!auth.isAdmin()) {
+      return alert('Este usuário não possui permissão para postar.');
+    }
+
+    window.location.hash = 'post';
   }
 
   sessionLogout(context) {
